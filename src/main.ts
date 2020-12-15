48cae6804c6e3b8602397edb85ecb0853766f8db
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2020-12-15 18:52:55 +0800

    add starting log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8085);
+  await app.listen(8085, () => {
+    Logger.log('Questionnaire Service is running on port 8085!');
+  });
 }
 bootstrap();
